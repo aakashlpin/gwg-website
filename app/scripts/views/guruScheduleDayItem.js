124a21178dfbdaed73258914a57e199d8d280c20
Aakash Goel <aakash.lpin@gmail.com>
2014-02-05 05:44:24 +0530

    add time slots

--- app/scripts/views/guruScheduleDayItem.js
+++ app/scripts/views/guruScheduleDayItem.js
@@ -8,7 +8,8 @@ var Marionette = require('backbone.marionette'),
     _ = require('underscore'),
     App = require('../main'),
     Backbone = require('backbone'),
-    TimeSlotView = require('./guruScheduleTimeSlot');
+    TimeSlotView = require('./guruScheduleTimeSlot'),
+    moment = require('moment');
 
 module.exports = Marionette.ItemView.extend({
     className: 'day-slots-container',
@@ -19,15 +20,57 @@ module.exports = Marionette.ItemView.extend({
     ui: {
         daySlotsContainer: '.daySlotsContainer'
     },
+    initialize: function () {
+        this.subViews = {};
+    },
     onRender: function () {
         this._initSlot();
     },
     _initSlot: function () {
-        this.ui.daySlotsContainer.html(new TimeSlotView().render().el);
+        var startTime = '09:00 AM',
+            endTime = '10:00 AM';
+        
+        this.ui.daySlotsContainer.html(this._getTimeSlotView(startTime, endTime).render().el);
 
+    },
+    _getTimeSlotView: function (startTime, endTime) {
+        return this.subViews[endTime] = new TimeSlotView({
+            model: this._addSlot(startTime, endTime)
+        });
+
+    },
+    _addSlot: function (startTime, endTime) {
+        if (!(this.model.get('slots') instanceof Backbone.Collection)) {
+            this.model.set('slots', new Backbone.Collection());
+        }
+
+        var slotToAdd = this._getModelForTimeSlot(startTime, endTime);
+        this.model.get('slots').add(slotToAdd);
+
+        return slotToAdd;
+
+    },
+    _getModelForTimeSlot: function (startTime, endTime) {
+        return new Backbone.Model({
+            date_startTime: moment(startTime, 'hh:mm A'),
+            date_endTime: moment(endTime, 'hh:mm A'),
+            startTime: startTime,
+            endTime: endTime
+        });
     },
     actionOnAddNewTimeSlot: function (e) {
         e.preventDefault();
+        var takenSlots = this.model.get('slots');
+        var minHour = takenSlots.max(function (takenSlot) {
+            return takenSlot.get('date_endTime').hours();
+        });
 
+        var startTime = minHour.get('endTime'),
+            endTime = minHour.get('date_endTime').add('hours', 1).format('hh:mm A');
+
+        //undo changes done to the original object
+        minHour.get('date_endTime').subtract('hours', 1);
+
+        this.ui.daySlotsContainer.append(this._getTimeSlotView(startTime, endTime).render().el);
     }
 });
--- app/scripts/views/guruScheduleTimeSlot.js
+++ app/scripts/views/guruScheduleTimeSlot.js
@@ -9,15 +9,35 @@ var Marionette = require('backbone.marionette'),
     App = require('../main'),
     Backbone = require('backbone'),
     $ = require('jquery'),
-    TimePicker = require('bootstrap.timepicker');
+    TimePicker = require('bootstrap.timepicker'),
+    moment = require('moment');
 
 module.exports = Marionette.ItemView.extend({
     template: templates.guruScheduleTimeSlot,
-    templateHelpers: function () {
-        if (this.model) return {};
-        return {
-            startTime: '08:00 AM',
-            endTime: '10:00 AM'
-        }
+    events: {
+        'change.bfhtimepicker': 'actionOnChangeTime'
+    },
+    onRender: function () {
+        var timepicker = this.$el.find('div.bfh-timepicker');
+        var _this = this;
+        timepicker.each(function() {
+            var _timepicker = $(this);
+            _timepicker.bfhtimepicker({
+                icon: '',
+                time: _this.model.get(_timepicker.data('type')),
+                mode: '12h'
+            });
+        });
+
+    },
+    actionOnChangeTime: function (e) {
+        var target = $(e.target),
+            targetType = target.data('type'),
+            targetValue = target.find('.bfh-timepicker-toggle input').val();
+
+        var modelChanges = {};
+        modelChanges[targetType] = targetValue;
+        modelChanges['date_' + targetType] = moment(targetValue, 'hh:mm A');
+        this.model.set(modelChanges);
     }
 });

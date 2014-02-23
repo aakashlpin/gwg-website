6f49797d9f41f549e046d5b441f19b9aa98d012d
Aakash Goel <aakash.lpin@gmail.com>
2014-02-23 20:49:00 +0530

    delete junk filed

--- scripts/vent.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by aakash on 2/5/14.
- */
-
-var Backbone = require('backbone'),
-    Marionette = require('backbone.marionette');
-
-module.exports = new Backbone.Wreqr.EventAggregator();

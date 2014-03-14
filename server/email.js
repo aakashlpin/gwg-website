8a042ded9d7f237d597498bb5bdd023f69caf5df
Aakash Goel <aakash.lpin@gmail.com>
2014-03-15 05:10:34 +0530

    email testing

---
+++ server/email.js
@@ -0,0 +1,53 @@
+var path           = require('path')
+    , templatesDir   = path.resolve(__dirname, '..', 'templates')
+    , emailTemplates = require('email-templates')
+    , nodemailer     = require('nodemailer');
+
+emailTemplates(templatesDir, function(err, template) {
+
+    if (err) {
+        console.log(err);
+    } else {
+
+        // ## Send a single email
+
+        // Prepare nodemailer transport object
+        var transport = nodemailer.createTransport("SMTP", {
+            service: "Gmail",
+            auth: {
+                user: "aakash@guitarwith.guru",
+                pass: "t1mguitarwithgurupasswd"
+            }
+        });
+
+        // An example users object with formatted email function
+        var locals = {
+            email: 'aakash.lpin@gmail.com',
+            guru_name: 'Aakash Goel',
+            image_root: 'http://guitarwith.guru/emails/welcome_guru/images'
+        };
+
+        // Send a single email
+        template('welcome_guru', locals, function(err, html, text) {
+            if (err) {
+                console.log(err);
+            } else {
+                transport.sendMail({
+                    from: 'Guitar with Guru <aakash@guitarwith.guru>',
+                    to: locals.email,
+                    subject: 'Hey from Guitar with Guru!',
+                    html: html,
+                    generateTextFromHTML: true,
+                    text: text
+                }, function(err, responseStatus) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log(responseStatus.message);
+                    }
+                });
+            }
+        });
+    }
+});
+
